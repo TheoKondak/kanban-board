6302f9de7dee81023d1121e3e43bd78cc8ffb6f5
TheoKondak <thkondak@gmail.com>
2023-02-25 17:38:55 +0100

    fix: fixed an interface related error

--- src/components/Ticket.tsx
+++ src/components/Ticket.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { EditText, EditTextarea } from 'react-edit-text';
 import 'react-edit-text/dist/index.css';
 
-const Tickets: React.FC<Card> = ({ title, content }) => (
+const Tickets: React.FC<Ticket> = ({ title, content }) => (
   <li>
     <h4>
       <EditText defaultValue={title} />
