a0af6c79ee79f25dc37ea11b668494a735d49c85
TheoKondak <thkondak@gmail.com>
2023-02-25 16:27:18 +0100

    feat: added content to task card

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
-const ListItem: React.FC<Card> = ({}) => {
-  return <li>ListItem</li>;
-};
-
+const ListItem: React.FC<Card> = ({ title, content }) => (
+  <li>
+    <h4>{title}</h4>
+    <p>{content}</p>
+  </li>
+);
 export default ListItem;
