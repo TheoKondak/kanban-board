aa468f4e83ccad7adbf306b23d4dd414d603d597
TheoKondak <thkondak@gmail.com>
2023-02-26 14:55:18 +0100

    chore: added id field to KanbanColumn & Ticket fields in the DB, will help with Drag n Drop. Removed generateId

--- src/components/Tickets.tsx
+++ src/components/Tickets.tsx
@@ -9,9 +9,9 @@ import generateId from '../helper/generateId';
 const Tickets: React.FC<Ticket[]> = ({ tickets }) => {
   return (
     <ul className="w-full my-0">
-      {tickets.map((ticket: Ticket, index: string) => {
-        const key = generateId(index.toString() + '_' + ticket.title.toString());
-        return <Ticket id={key} key={key} title={ticket.title} content={ticket.content} />;
+      {tickets.map((ticket: Ticket) => {
+        // const key = generateId(index.toString() + '_' + ticket.title.toString());
+        return <Ticket id={ticket.id.toString()} key={ticket.id.toString()} title={ticket.title} content={ticket.content} />;
       })}
     </ul>
   );
