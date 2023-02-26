638edba5fb8b663b2b09b82a87fdbc8a744432c7
TheoKondak <thkondak@gmail.com>
2023-02-26 11:15:24 +0100

    feat: added some responsiveness, handled overflows

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App = () => {
   const generateId = (data: string) => `${data}_${new Date().getTime()}`;
 
   return (
-    <div className="App bg-[rgb(36,36,36)] flex flex-col text-left md:flex-row  justify-evenly items-center mx-1 md:mx-5">
+    <div className="App bg-[rgb(36,36,36)]  text-left mx-4 md:mx-10 md:my-4 p-y-10 w-full flex space-x-5 overflow-x-scroll overflow-y-hidden ">
       {kanbanColumns ? (
         kanbanColumns.map((kanbanColumn: KanbanColumn, index: number) => {
           const key: string = generateId(index.toString() + '_' + kanbanColumn.title.toString());
