fc863a370f7dd2c509809ef0af4f2074a701acea
TheoKondak <thkondak@gmail.com>
2023-02-28 12:34:00 +0100

    chore: some modal styling

--- src/components/modal/TicketModal.tsx
+++ src/components/modal/TicketModal.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import { VscCloseAll } from 'react-icons/vsc';
+
 interface TicketModal {
   onBackdropClick: Function;
 }
@@ -10,16 +12,16 @@ const TicketModal: React.FC<TicketModal> = ({ onBackdropClick, isTicketModalVisi
   return ReactDom.createPortal(
     <div>
       <div className={`fixed inset-0 bg-black ${isTicketModalVisible ? 'opacity-50 z-0' : 'pointer-events-none opacity-0'}`} onClick={onBackdropClick}></div>
-      <div className={`fixed inset-1/4 h-2/3 bg-white text-black shadow-lg w-full max-w-screen-sm p-4 ${isTicketModalVisible ? 'opacity-100' : 'pointer-events-none opacity-0'}`}>
+      <div className={`fixed inset-1/4  w-6/12  h-2/3 bg-white text-black shadow-lg max-w-screen-sm p-4 ${isTicketModalVisible ? 'opacity-100' : 'pointer-events-none opacity-0'}`}>
         <div className="modalHeader relative h-5">
-          <button className="text-white absolute right-0 top:0" onClick={onBackdropClick}>
-            Close
+          <button className="text-white absolute right-0 -top-2 p-0" onClick={onBackdropClick}>
+            <VscCloseAll />
           </button>
         </div>
 
         <div className="">
-          <h4>Some Title</h4>
-
+          <h4 className="">Some Title</h4>
+          <hr className=" block my-2 " />
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita ab blanditiis corporis accusamus asperiores odit voluptatibus illum assumenda atque sit? Qui fuga obcaecati officia, officiis hic magnam sequi fugiat ex quaerat id atque recusandae eaque voluptas quia dignissimos, nihil labore iusto reprehenderit? Fugiat praesentium vero dolores, ipsam ullam odit molestias quibusdam doloremque voluptas commodi ex. Similique deserunt distinctio nobis, sed neque consectetur repellendus vero aliquid fugit.</p>
         </div>
       </div>
