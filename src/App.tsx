c581ea4812594459fc73f7b9bc817e53eb4c88db
TheoKondak <thkondak@gmail.com>
2023-02-28 23:19:53 +0100

    chore: moved some state, and GETs to App.js

--- src/App.tsx
+++ src/App.tsx
@@ -16,20 +16,31 @@ import './App.css';
 
 const App = () => {
   const [isTicketModalVisible, setIsTicketModalVisible] = useState(false);
+  const [kanbanColumns, setKanbanColumns] = useState<Kanban[] | null>(null);
+  const [tickets, setTickets] = useState<Tickets | null>(null);
+
+  useEffect(() => {
+    kanbanService.get('/tickets').then((tickets) => {
+      setTickets(tickets);
+    });
+
+    kanbanService.get('/columns').then((columns) => {
+      setKanbanColumns(columns);
+    });
+  }, []);
 
   let location = useLocation();
   let state = location.state as { backgroundLocation?: Location };
 
   const triggerTicketModal = () => {
-    console.log('onClick={triggerTicketModal}');
     setIsTicketModalVisible((wasTicketModalVisible) => !wasTicketModalVisible);
   };
 
   return (
     <div className="App bg-[rgb(36,36,36)]  text-left mx-4 md:mx-10 md:my-4 p-y-10 w-full flex flex-column space-x-5 overflow-x-scroll overflow-y-hidden ">
       <Routes location={state?.backgroundLocation || location}>
-        <Route path="/" element={<Root triggerTicketModal={triggerTicketModal} />}>
-          <Route path="/tickets/:id" element={<BaseModalWrapper onBackdropClick={triggerTicketModal} isTicketModalVisible={isTicketModalVisible} />} />
+        <Route path="/" element={<Root triggerTicketModal={triggerTicketModal} kanbanColumns={kanbanColumns} tickets={tickets} setTickets={setTickets} />}>
+          <Route path="/tickets/:ticketId" element={<BaseModalWrapper onBackdropClick={triggerTicketModal} isTicketModalVisible={isTicketModalVisible} tickets={tickets} />} />
           <Route path="*" element={<ErrorPage />} />
         </Route>
       </Routes>
@@ -37,7 +48,7 @@ const App = () => {
       {/* Show the modal when a `backgroundLocation` is set */}
       {state?.backgroundLocation && (
         <Routes>
-          <Route path="/tickets/:id" element={<BaseModalWrapper onBackdropClick={triggerTicketModal} isTicketModalVisible={isTicketModalVisible} />} />
+          <Route path="/tickets/:ticketId" element={<BaseModalWrapper onBackdropClick={triggerTicketModal} isTicketModalVisible={isTicketModalVisible} />} />
         </Routes>
       )}
     </div>
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -17,20 +17,7 @@ import { Outlet, Link } from 'react-router-dom';
 // CSS
 import '../App.css';
 
-const Root = ({ triggerTicketModal }) => {
-  const [kanbanColumns, setKanbanColumns] = useState<Kanban[] | null>(null);
-  const [tickets, setTickets] = useState<Tickets | null>(null);
-
-  useEffect(() => {
-    kanbanService.get('/tickets').then((tickets) => {
-      setTickets(tickets);
-    });
-
-    kanbanService.get('/columns').then((columns) => {
-      setKanbanColumns(columns);
-    });
-  }, []);
-
+const Root = ({ triggerTicketModal, kanbanColumns, tickets, setTickets }) => {
   return (
     <div className="App bg-[rgb(36,36,36)]  text-left mx-4 md:mx-10 md:my-4 p-y-10 w-full flex space-x-5 overflow-x-scroll overflow-y-hidden ">
       <DndProvider backend={HTML5Backend}>
