ab0123583369d75a193f1cb91b62138b90322e9e
TheoKondak <thkondak@gmail.com>
2023-03-02 16:20:29 +0100

    fix: fixed an issue with Ticket content - The three dots only appear if the text length is larger than the one set on settings.

--- src/components/Ticket.tsx
+++ src/components/Ticket.tsx
@@ -42,7 +42,7 @@ const Ticket: React.FC<Ticket> = ({ ticketId, title, content, columnId, setTicke
           {title}
         </h4>
 
-        <div className="text-[9px] text-white font-light pb-2">{content.length > 0 ? `${content.substring(0, settings.tickets.ticketPreviewLength)}...` : ''}</div>
+        <div className="text-[9px] text-white font-light pb-2">{content.length > settings.tickets.ticketPreviewLength ? `${content.substring(0, settings.tickets.ticketPreviewLength)}...` : content}</div>
       </Link>
     </li>
   );
