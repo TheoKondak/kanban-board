55704b7660247a4de0f35a6cad56edca23211c64
TheoKondak <thkondak@gmail.com>
2023-03-03 13:41:12 +0100

    feat: JSON-server URL can be changed in .env A static default value is available in case .env is not

--- src/services/kanbanService.tsx
+++ src/services/kanbanService.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3000';
+const baseUrl = import.meta.env.VITE_SERVER_URL || 'http://localhost:3000';
 
 const get: Get = (query) => {
   const request = axios.get(baseUrl + query);
