2b8676eb9a516ea151240b45bf47d623ca73a758
TheoKondak <thkondak@gmail.com>
2023-03-06 16:46:24 +0100

    fix: When there are no tickets in the column, renders a message

--- src/components/Tickets.tsx
+++ src/components/Tickets.tsx
@@ -7,12 +7,19 @@ const Tickets: React.FC<Ticket[]> = ({ columnTickets, tickets, setTickets, trigg
     setTickets(updatedTickets);
   };
 
+  console.log(columnTickets);
   return (
-    <ul className={'tickets w-full my-0 overflow-x-auto '}>
-      {columnTickets.map((ticket: Ticket) => {
-        return <Ticket ticketId={ticket.id} key={ticket.id.toString()} title={ticket.title} content={ticket.content} moveTicket={moveTicket} setTickets={setTickets} tickets={tickets} triggerTicketModal={triggerTicketModal} settings={settings} />;
-      })}
-    </ul>
+    <div>
+      {columnTickets.length > 0 ? (
+        <ul className={'tickets w-full my-0 overflow-x-auto '}>
+          {columnTickets.map((ticket: Ticket) => {
+            return <Ticket ticketId={ticket.id} key={ticket.id.toString()} title={ticket.title} content={ticket.content} moveTicket={moveTicket} setTickets={setTickets} tickets={tickets} triggerTicketModal={triggerTicketModal} settings={settings} />;
+          })}
+        </ul>
+      ) : (
+        <div className="no-tickets bg-primary-500 dark:bg-primary-700 rounded p-4 mx-2 mb-2 text-sm text-center text-white font-thin ">No tickets in this column</div>
+      )}
+    </div>
   );
 };
 
